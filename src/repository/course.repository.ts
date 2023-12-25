d87e084631d16652fd02d7add111840a3ba2479c
AntonMiranovich <antonmir992@gmail.com>
2023-12-25 21:26:06 +0300

    task1231236631

--- src/repository/course.repository.ts
+++ src/repository/course.repository.ts
@@ -15,4 +15,49 @@ async function getCourseByIdDB(id: number): Promise<iCourse[]> {
   return data;
 }
 
-export { getAllCourseDB, getCourseByIdDB };
+async function postCourseDB(course: string): Promise<iCourse[]> {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const sql = 'insert into courses (course) values ($1) returning *';
+    const data = (await client.query(sql, [course])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error: any) {
+    await client.query('ROLLBACK');
+    console.log(`postCourseDB:${error.message}`);
+    return [];
+  }
+}
+
+async function updateCourseDB(id: number, course: string): Promise<iCourse[]> {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const sql = 'update courses set course=$1 where id=$2 returning*';
+    const data = (await client.query(sql, [course, id])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error: any) {
+    await client.query('ROLLBACK');
+    console.log(`updateCourseDB:${error.message}`);
+    return [];
+  }
+}
+
+async function deleteCourseByIdDB(id: number): Promise<iCourse[]> {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const sql = `delete from courses where id=$1 returning *`;
+    const data = (await client.query(sql, [id])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error: any) {
+    await client.query('ROLLBACK');
+    console.log(`deleteCourseByIdDB:${error.message}`);
+    return [];
+  }
+}
+
+export { getAllCourseDB, getCourseByIdDB, postCourseDB, updateCourseDB, deleteCourseByIdDB };
--- src/service/course.service.ts
+++ src/service/course.service.ts
@@ -1,17 +1,35 @@
 import ExceptionType from '../helper/exception';
 import { iCourse } from '../interfaces/interfaces';
-import {getAllCourseDB,getCourseByIdDB} from '../repository/course.repository'
+import { getAllCourseDB, getCourseByIdDB, postCourseDB, updateCourseDB, deleteCourseByIdDB } from '../repository/course.repository';
 
-async function getAllCourse():Promise<iCourse[]> {
-  const data =await getAllCourseDB();
+async function getAllCourse(): Promise<iCourse[]> {
+  const data = await getAllCourseDB();
   if (!data.length) throw new Error('no data');
   return data;
 }
 
-async function getCourseById(id:number):Promise<iCourse[]>{
-    const data =await getCourseByIdDB(id);
-    if (!data.length) throw new Error('no data');
-    return data;
+async function getCourseById(id: number): Promise<iCourse[]> {
+  const data = await getCourseByIdDB(id);
+  if (!data.length) throw new Error('no data');
+  return data;
 }
 
-export { getAllCourse,getCourseById };
+async function postCourse(course: string): Promise<iCourse[]> {
+  const data = await postCourseDB(course);
+  if (!data.length) throw new Error('curse is not post');
+  return data;
+}
+
+async function updateCourse(id: number, course: string): Promise<iCourse[]> {
+  const data = await updateCourseDB(id, course);
+  if (!data.length) throw new Error('curse is not update');
+  return data;
+}
+
+async function deleteCourseById(id: number): Promise<iCourse[]> {
+  const data = await deleteCourseByIdDB(id);
+  if (!data.length) throw new Error('curse is not delete');
+  return data;
+}
+
+export { getAllCourse, getCourseById, postCourse, updateCourse, deleteCourseById };
