9f304b3722caa99f9946b4f5f22f7f1fb07bd0b2
AntonMiranovich <antonmir992@gmail.com>
2024-01-10 14:15:34 +0300

    tasks 12251231312312

--- src/tests/controller/api.controller.test.ts
+++ src/tests/controller/api.controller.test.ts
@@ -1,9 +1,11 @@
 import request from 'supertest';
 import app from '../../app';
+import { match } from 'assert';
+
+const randomEmail = Math.floor(Math.random() * 100000);
 
 test('/api/reg', async () => {
-
-  const res = await request(app).post('/api/reg').send({name: 'anton',surname: 'mir',email: 'a12213122331223123@gmail.com',pwd: 'wqeddasd',});
+  const res = await request(app).post('/api/reg').send({ name: 'anton', surname: 'mir', email: `${randomEmail}@gmail.com`, pwd: 'wqeddasd' });
 
   expect(res.statusCode).toBe(200);
   expect(res.body).toBeTruthy();
@@ -11,8 +13,7 @@ test('/api/reg', async () => {
 });
 
 test('/api/auth', async () => {
-
-  const res = await request(app).post('/api/auth').send({email: 'a12213122331223123@gmail.com',pwd: 'wqeddasd',});
+  const res = await request(app).post('/api/auth').send({ email: `${randomEmail}@gmail.com`, pwd: 'wqeddasd' });
 
   expect(res.statusCode).toBe(200);
   expect(res.body).toBeTruthy();
