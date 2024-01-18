e602f1179e9720c841f93e6a920154699e02537f
AntonMiranovich <antonmir992@gmail.com>
2024-01-18 23:38:34 +0300

    ta2113212

--- Server/src/tests/controller/api.controller.test.ts
+++ Server/src/tests/controller/api.controller.test.ts
@@ -3,16 +3,18 @@ import app from '../../app';
 
 test('/api/reg', async () => {
 
-  const res = await request(app).post('/api/reg').send({name: 'anton',surname: 'mir',email: 'a12213122331223123@gmail.com',pwd: 'wqeddasd',});
+  const res = await request(app).post('/api/reg').send({name: 'anton',surname: 'mir',email: '3312@gmail.com',pwd: 'wqeddasd',});
 
   expect(res.statusCode).toBe(200);
   expect(res.body).toBeTruthy();
   expect(res.body.length).toBeGreaterThanOrEqual(0);
 });
 
+
+
 test('/api/auth', async () => {
 
-  const res = await request(app).post('/api/auth').send({email: 'a12213122331223123@gmail.com',pwd: 'wqeddasd',});
+  const res = await request(app).post('/api/auth').send({email: '3312@gmail.com',pwd: 'wqeddasd',});
 
   expect(res.statusCode).toBe(200);
   expect(res.body).toBeTruthy();
