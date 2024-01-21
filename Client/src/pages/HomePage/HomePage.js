c877bbbc923021878177740c2fa5ebad03257d1c
AntonMiranovich <antonmir992@gmail.com>
2024-01-21 22:06:14 +0300

    educationPlatform01

--- Client/src/pages/HomePage/HomePage.js
+++ Client/src/pages/HomePage/HomePage.js
@@ -1,11 +1,13 @@
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Preview from "../../components/Preview/Preview";
 
 function HomePage() {
   return (
     <div>
-      <Header></Header>
-      <Preview></Preview>
+      <Header />
+      <Preview />
+      <Footer />
     </div>
   );
 }
