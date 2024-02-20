1d936386a5167856cf93274ef12125c2ea8d2fca
AntonMiranovich <antonmir992@gmail.com>
2024-02-20 21:48:33 +0300

    refactor project

--- Client/src/components/Authorization/Authorization.js
+++ Client/src/components/Authorization/Authorization.js
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom";
 import style from "./style.module.css";
+import Input from "../Input/Input";
 
 function Authorization() {
+  const inpArr = [
+    {
+      id: 1,
+      placeholdervalue: "Enter Email...",
+      inpType: "text",
+    },
+    {
+      id: 2,
+      placeholdervalue: "Enter Password...",
+      inpType: "password",
+    },
+  ];
+
   return (
     <div className={style.wrapper}>
       <div className={style.login}>
         <h1>Login</h1>
-        <input placeholder="Placeholder text"></input>
-        <input placeholder="Placeholder text"></input>
+        {inpArr.map((el) => (
+          <Input el={el} />
+        ))}
         <Link to={"/listcourses"}>
           <div className={style.btnLogin}>Login</div>
         </Link>
