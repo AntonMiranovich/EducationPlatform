81e703b8a7abc372f69954cf2a22d3917d5b71aa
AntonMiranovich <antonmir992@gmail.com>
2024-02-21 19:05:55 +0300

    refactor project client

--- Client/src/pages/AuthorizationPage/AuthorizationPage.js
+++ Client/src/pages/AuthorizationPage/AuthorizationPage.js
@@ -1,12 +1,40 @@
-import Authorization from "../../components/Authorization/Authorization";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import { Link } from "react-router-dom";
+import style from "./style.module.css";
+import Input from "../../components/Input/Input";
 
 function AuthorizationPage() {
+  const inpArr = [
+    {
+      id: 1,
+      placeholdervalue: "Enter Email...",
+      inpType: "text",
+    },
+    {
+      id: 2,
+      placeholdervalue: "Enter Password...",
+      inpType: "password",
+    },
+  ];
+
   return (
     <>
       <Header />
-      <Authorization />
+
+      <div className={style.wrapper}>
+        <div className={style.login}>
+          <h1>Login</h1>
+          {inpArr.map((el, index) => (
+            <Input key={index} el={el} />
+          ))}
+          <Link to={"/listcourses"}>
+            <div className={style.btnLogin}>Login</div>
+          </Link>
+        </div>
+        <div className={style.img}></div>
+      </div>
+
       <Footer />
     </>
   );
--- Client/src/pages/RegistrationPage/RegistrationPage.js
+++ Client/src/pages/RegistrationPage/RegistrationPage.js
@@ -1,12 +1,54 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
-import Registration from "../../components/Registration/Registration";
+import { Link } from "react-router-dom";
+import style from "./style.module.css";
+import Input from "../../components/Input/Input";
 
 function RegistrationPage() {
+  const inpArr = [
+    {
+      id: 1,
+      placeholdervalue: "Enter Name...",
+      inpType: "text",
+    },
+    {
+      id: 2,
+      placeholdervalue: "Enter Surname...",
+      inpType: "text",
+    },
+    {
+      id: 3,
+      placeholdervalue: "Enter Email...",
+      inpType: "text",
+    },
+    {
+      id: 4,
+      placeholdervalue: "Enter Password...",
+      inpType: "password",
+    },
+    {
+      id: 5,
+      placeholdervalue: "Enter ConfirmPassword...",
+      inpType: "password",
+    },
+  ];
+
   return (
     <>
       <Header />
-      <Registration />
+      <div className={style.wrapper}>
+        <div className={style.singUp}>
+          <h1>Sign Up</h1>
+          {inpArr.map((el,index) => (
+            <Input key={index} el={el} />
+          ))}
+
+          <div className={style.btnSingUp}>
+            <Link to={"/listcourses"}>Sign Up </Link>
+          </div>
+        </div>
+        <div className={style.img}></div>
+      </div>
       <Footer />
     </>
   );
